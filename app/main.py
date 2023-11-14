14e9b769e2b674c1ddc14b40f1262a0480d569b2
ffilippello <filippello@gmail.com>
2023-11-14 05:22:35 -0300

    test whitlist

--- app/main.py
+++ app/main.py
@@ -40,8 +40,8 @@ async def convert_audio(file: UploadFile):
     """
     save_path = os.path.join("app", file.filename)
     try:
-        listFormats = ["audio/wav","audio/mpeg","audio/mp4"]
-        if (file.content_type not in listFormats):
+        #listFormats = ["audio/wav","audio/mpeg","audio/mp4"]
+        if (file.content_type != "audio/wav"):
            return Response("Error, el audio tiene q ser wav", 400)
         
         # Guardar el archivo de audio con su nombre original
